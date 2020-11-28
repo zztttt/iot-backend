82d087aa8040c09eb52255bd7ea6adead9de25af
zztttt <1315691850@qq.com>
2020-11-29 02:25:09 +0800

    update upLoadFile service

--- src/main/java/com/iot/IotApplication.java
+++ src/main/java/com/iot/IotApplication.java
@@ -1,13 +1,27 @@
 package com.iot;
 
+import com.iot.service.StorageProperties;
+import com.iot.service.StorageService;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class IotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IotApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
--- src/main/java/com/iot/controller/GreetingController.java
+++ src/main/java/com/iot/controller/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class greetingController {
+public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
